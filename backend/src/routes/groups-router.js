b3581590bb1630b1a1ce6776a17f1c2c5b155d17
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-15 16:05:00 -0800

    router accepts multiple query parameters and returns correct results

--- backend/src/routes/groups-router.js
+++ backend/src/routes/groups-router.js
@@ -11,16 +11,16 @@ const {
 const groupsRouter = express.Router();
 
 groupsRouter.get('/', async (req, res) => {
-  let result;
   const { campus, demographic, group_type, meeting_date, zip_code } = req.query;
   try {
     const response = await axios.get(DATABASE_URL);
     const { data } = response;
-    if (campus) result = filterCampus(data, campus);
-    if (demographic) result = filterDemographic(data, demographic);
-    if (group_type) result = filterGroupType(data, group_type);
-    if (meeting_date) result = filterMeetingDate(data, meeting_date);
-    if (zip_code) result = filterZipCode(data, zip_code);
+    let result = data;
+    if (campus) result = filterCampus(result, campus);
+    if (demographic) result = filterDemographic(result, demographic);
+    if (group_type) result = filterGroupType(result, group_type);
+    if (meeting_date) result = filterMeetingDate(result, meeting_date);
+    if (zip_code) result = filterZipCode(result, zip_code);
     res.status(200).send(result);
   } catch (error) {
     res.status(404).send(error);
