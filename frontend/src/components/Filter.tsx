79185e736c1a13fad551767eb8239ee3a4b680ce
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-03-01 14:07:40 -0800

    splits Filter into FilterDisplay

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -1,21 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Paper,
-  Select,
-  MenuItem,
-  Typography,
-  filters,
-  FaChild,
-  KeyboardArrowDownIcon,
-  getQueryString,
-  getParams,
-  smartDate,
-  Switch,
-  FilterKey,
-} from '../utils/index';
-import { BootstrapInput, useStyles } from './FilterStyles';
+import { getQueryString, getParams, FilterKey } from '../utils/index';
+import { FilterDisplay } from './FilterDisplay';
 
-type FormValues = {
+export type FormValues = {
   campus: string;
   demographic: string;
   group_type: string;
@@ -42,14 +29,7 @@ export const Filter: React.FC<FilterProps> = ({ getResults }) => {
   let url = 'http://localhost:8000/groups/?';
 
   function handleChange(key: FilterKey, value: unknown) {
-    // Switch on filter type
     setFormFields({ ...formFields, [key]: value });
-
-    // if (name === childCareKey)
-    //   setFormFields({ ...formFields, [name]: !formFields.child_care });
-    // if (value === 'View All' && name) setFormFields({ ...formFields, [name]: '' });
-    // if (value !== 'View All' && name && name !== childCareKey)
-    //   setFormFields({ ...formFields, [name]: value });
   }
 
   useEffect(() => {
@@ -59,61 +39,4 @@ export const Filter: React.FC<FilterProps> = ({ getResults }) => {
   }, [formFields, getResults, url]);
 
   return <FilterDisplay formFields={formFields} handleChange={handleChange} />;
-};
-
-export type FilterDisplayProps = {
-  formFields: FormValues;
-  handleChange: (key: FilterKey, value: unknown) => void;
-};
-
-export const FilterDisplay: React.FC<FilterDisplayProps> = function FilterDisplay(
-  props
-) {
-  const classes = useStyles();
-  return (
-    <Paper className={classes.root}>
-      {filters.map((filter, i) => (
-        <div key={i} className={classes.input}>
-          <Typography variant='subtitle2'>{filter.title}</Typography>
-          <Select
-            labelId=''
-            id='demo-simple-select'
-            onChange={(e) => props.handleChange(filter.value, e.target.value)}
-            defaultValue=''
-            value={props.formFields[filter.value]}
-            input={<BootstrapInput />}
-            name={filter.value}
-            IconComponent={KeyboardArrowDownIcon}
-          >
-            <MenuItem value=''>View all</MenuItem>
-            {filter.options.map((option, i) => (
-              <MenuItem value={option.value} key={i}>
-                {smartDate(option.name)}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
-      ))}
-      <Typography variant='subtitle2'>Zip Code</Typography>
-      <BootstrapInput
-        id='bootstrap-input'
-        name='zip_code'
-        className={classes.input}
-        onChange={(e) => props.handleChange('zip_code', e.target.value)}
-      />
-      <div className={classes.flex}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <FaChild className={classes.icon} />
-          <Typography variant='h5'>Child care provided</Typography>
-        </div>
-        <Switch
-          checked={props.formFields['child_care']}
-          onChange={(e) => props.handleChange('child_care', e.target.value)}
-          name='child_care'
-          color='primary'
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
-      </div>
-    </Paper>
-  );
 };
---
+++ frontend/src/components/FilterDisplay.tsx
@@ -0,0 +1,72 @@
+import {
+  Paper,
+  Select,
+  MenuItem,
+  Typography,
+  filters,
+  FaChild,
+  KeyboardArrowDownIcon,
+  smartDate,
+  Switch,
+  FilterKey,
+} from '../utils/index';
+import { FormValues } from './Filter';
+import { BootstrapInput, useStyles } from './FilterStyles';
+
+export type FilterDisplayProps = {
+  formFields: FormValues;
+  handleChange: (key: FilterKey, value: unknown) => void;
+};
+
+export const FilterDisplay: React.FC<FilterDisplayProps> = function FilterDisplay(
+  props
+) {
+  const classes = useStyles();
+  
+  return (
+    <Paper className={classes.root}>
+      {filters.map((filter, i) => (
+        <div key={i} className={classes.input}>
+          <Typography variant='subtitle2'>{filter.title}</Typography>
+          <Select
+            labelId=''
+            id='demo-simple-select'
+            onChange={(e) => props.handleChange(filter.value, e.target.value)}
+            defaultValue=''
+            value={props.formFields[filter.value]}
+            input={<BootstrapInput />}
+            name={filter.value}
+            IconComponent={KeyboardArrowDownIcon}
+          >
+            <MenuItem value=''>View all</MenuItem>
+            {filter.options.map((option, i) => (
+              <MenuItem value={option.value} key={i}>
+                {smartDate(option.name)}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+      ))}
+      <Typography variant='subtitle2'>Zip Code</Typography>
+      <BootstrapInput
+        id='bootstrap-input'
+        name='zip_code'
+        className={classes.input}
+        onChange={(e) => props.handleChange('zip_code', e.target.value)}
+      />
+      <div className={classes.flex}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <FaChild className={classes.icon} />
+          <Typography variant='h5'>Child care provided</Typography>
+        </div>
+        <Switch
+          checked={props.formFields['child_care']}
+          onChange={(e) => props.handleChange('child_care', e.target.value)}
+          name='child_care'
+          color='primary'
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
+      </div>
+    </Paper>
+  );
+};
